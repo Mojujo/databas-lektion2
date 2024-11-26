6c4bf1802c7f9ee5a8678d42fed53e8836795cf1
Mojujo <oscar.tornqvist01@gmail.com>
2024-11-26 13:09:29 +0100

    Initial commit

---
+++ src/main/java/se/oscar/utility/loggerUtil.java
@@ -0,0 +1,16 @@
+package se.oscar;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggerUtil {
+    private static final Logger logger = LoggerFactory.getLogger(LoggerUtil.class);
+
+    private LoggerUtil() {
+        // Private constructor to prevent instantiation
+    }
+
+    public static void logError(String message, Throwable throwable) {
+        logger.error(message, throwable);
+    }
+}
