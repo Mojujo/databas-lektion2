8d68fbb7ad6cff3c76c162a87459557435a7dc8f
Mojujo <oscar.tornqvist01@gmail.com>
2024-12-05 09:32:35 +0100

    update

--- src/main/java/se/oscar/Main.java
+++ src/main/java/se/oscar/Main.java
@@ -8,10 +8,6 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        PersonDAO personDAO = new PersonDAOImpl();
-        PersonService service = new PersonService(personDAO);
-        Person person = new Person("Anna", "Törnqvist", "F", "1905-03-23", 12345);
 
-        service.addPerson(person);
     }
 }
